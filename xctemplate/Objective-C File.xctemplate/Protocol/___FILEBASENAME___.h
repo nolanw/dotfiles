555104f96675682205892ca737a600dfc2076a96
Nolan Waite <nolan@nolanw.ca>
2016-02-27 13:54:31 -0400

    Add an Xcode template.

---
+++ xctemplate/Objective-C File.xctemplate/Protocol/___FILEBASENAME___.h
@@ -0,0 +1,17 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+@import Foundation;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol ___FILEBASENAMEASIDENTIFIER___ <NSObject>
+
+@end
+
+NS_ASSUME_NONNULL_END
