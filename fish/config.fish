fd74b9e363e822148286053e8071cec02e08b963
Nolan Waite <nolan@nolanw.ca>
2016-10-13 15:08:27 -0300

    Sup fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,8 @@
+# Sync universal variables.
 set -qUx EDITOR; or set -Ux EDITOR mate -w
+
+# direnv
+eval (direnv hook fish)
+
+# Various package managers' bin folders
+set PATH (ruby -rubygems -e 'puts Gem.user_dir')/bin ~/Library/Python/2.7/bin $PATH
