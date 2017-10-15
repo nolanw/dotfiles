8d8c6441a0eb74ad929335cae7ac4c2afc10c69b
Nolan Waite <nolan@nolanw.ca>
2017-10-15 00:14:01 -0300

    Remove Postgres from path.
    Don't have it installed everywhere.

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,7 @@ set -qUx EDITOR; or set -Ux EDITOR 'mate -w'
 eval (direnv hook fish)
 
 # Various package managers' bin folders
-set -U fish_user_paths (ruby -rubygems -e 'puts Gem.user_dir')/bin ~/Library/Python/2.7/bin /Applications/Postgres.app/Contents/Versions/latest/bin $fish_user_paths
+set -U fish_user_paths (ruby -rubygems -e 'puts Gem.user_dir')/bin ~/Library/Python/2.7/bin $fish_user_paths
 
 # Local config
 set -l local_config_file ~/.local_config.fish
