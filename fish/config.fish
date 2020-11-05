24837c59c867f8359c42f05e9b0a3b9966d251d8
Nolan Waite <nolan@nolanw.ca>
2020-11-05 19:46:42 -0400

    Add pyenv and remove old python bits.

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,6 @@
 set -gx COCOAPODS_DISABLE_STATS true
 set -gx EDITOR bbedit --wait --resume
 set -gx FASTLANE_SKIP_UPDATE_CHECK 1
-set -gx PIP_USER yes
 
 # Sync universal variables (erase now, build up later)
 set -Ue fish_user_paths
@@ -14,15 +13,21 @@ end
 # homebrew ruby
 set -p fish_user_paths /usr/local/opt/ruby/bin
 
+# pyenv
+if type -q pyenv
+  set -gx PYENV_ROOT $HOME/.pyenv
+  set -p fish_user_paths $PYENV_ROOT/bin
+  if status --is-interactive
+    pyenv init - | source
+  end
+end
+
 # Various bin folders
 begin
   set -l gemhome (ruby -r rubygems -e 'puts Gem.user_dir')
   if test -e $gemhome
     set -a fish_user_paths $gemhome/bin
   end
-end
-if test -e ~/Library/Python/2.7/bin
-  set -a fish_user_paths ~/Library/Python/2.7/bin
 end
 if test -e /Applications/Postgres.app
   set -a fish_user_paths /Applications/Postgres.app/Contents/Versions/latest/bin
@@ -36,7 +41,7 @@ if status --is-interactive
 end
 
 # Local config
-set -l local_config_file ~/.local_config.fish
+set -l local_config_file $HOME/.local_config.fish
 if test -f $local_config_file
   source $local_config_file
 end
