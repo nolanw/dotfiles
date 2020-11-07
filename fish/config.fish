e5074e80ad2a58a05e6239ae4b2401a9902b3302
Nolan Waite <nolan@nolanw.ca>
2020-11-07 14:08:08 -0400

    Futz with paths and stash Terminal theme.

--- fish/config.fish
+++ fish/config.fish
@@ -2,46 +2,49 @@ set -gx COCOAPODS_DISABLE_STATS true
 set -gx EDITOR bbedit --wait --resume
 set -gx FASTLANE_SKIP_UPDATE_CHECK 1
 
-# Sync universal variables (erase now, build up later)
+# Paths
+
+## Reset user paths so we can build it back up
 set -Ue fish_user_paths
 
-# direnv
-if type -q direnv
-  eval (direnv hook fish)
-end
+## Go
+set -gx GOPATH $HOME/.go
+set -p fish_user_paths $GOPATH/bin
 
-# homebrew ruby
-set -p fish_user_paths /usr/local/opt/ruby/bin
+## MacPorts
+set -p fish_user_paths /opt/local/bin /opt/local/sbin
 
-# pyenv
+## pyenv
 if type -q pyenv
-  set -gx PYENV_ROOT $HOME/.pyenv
-  set -p fish_user_paths $PYENV_ROOT/bin
-  if status --is-interactive
-    pyenv init - | source
-  end
+    set -gx PYENV_ROOT $HOME/.pyenv
+    set -p fish_user_paths $PYENV_ROOT/bin
 end
 
-# Various bin folders
-begin
-  set -l gemhome (ruby -r rubygems -e 'puts Gem.user_dir')
-  if test -e $gemhome
-    set -a fish_user_paths $gemhome/bin
-  end
+
+# Hooks
+
+## direnv
+type -q direnv; and eval (direnv hook fish)
+
+## jump
+status --is-interactive; and source (jump shell fish | psub)
+
+## pyenv
+if type -q pyenv; and status --is-interactive
+    pyenv init - | source
 end
-if test -e /Applications/Postgres.app
-  set -a fish_user_paths /Applications/Postgres.app/Contents/Versions/latest/bin
-end
+
 
 # Abbreviations
+
 if status --is-interactive
-  abbr --add bb 'bbedit'
-  abbr --add b 'bundle exec'
-  abbr --add g 'git'
+    abbr --add b 'bundle exec'
+    abbr --add bb 'bbedit'
+    abbr --add g 'git'
 end
 
+
 # Local config
-set -l local_config_file $HOME/.local_config.fish
-if test -f $local_config_file
-  source $local_config_file
-end
+
+set -l local_config $HOME/.local_config.fish
+test -f $local_config; and source $local_config
