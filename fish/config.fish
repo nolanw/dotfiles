ec308ea7a6b3c98e03db667f2021a3c4b80f0c5a
Nolan Waite <nolan@nolanw.ca>
2023-05-30 17:26:19 -0300

    Reorganize fish config and move some local config into conditional global config.

--- fish/config.fish
+++ fish/config.fish
@@ -1,51 +1,3 @@
-set -gx EDITOR bbedit --wait --resume
-
-# Gunk
-
-set -gx COCOAPODS_DISABLE_STATS true
-set -gx FASTLANE_SKIP_UPDATE_CHECK 1
-set -gx HOMEBREW_NO_ANALYTICS 1
-
-
-# Paths
-
-## Reset user paths so we can build it back up
-set -Ue fish_user_paths
-
-## Local stuff
-set -p fish_user_paths $HOME/bin
-
-## Go
-set -gx GOPATH $HOME/.go
-set -p fish_user_paths $GOPATH/bin
-
-## Homebrew
-if test -d /opt/homebrew
-    set -gx HOMEBREW_PREFIX /opt/homebrew
-    set -p fish_user_paths $HOMEBREW_PREFIX/bin $HOMEBREW_PREFIX/sbin
-    set -gx HOMEBREW_CELLAR $HOMEBREW_PREFIX/Cellar
-    set -gx HOMEBREW_REPOSITORY $HOMEBREW_PREFIX
-    set -gx HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK 1
-end
-
-## pyenv
-if type -q pyenv
-    set -gx PYENV_ROOT $HOME/.pyenv
-    set -p fish_user_paths $PYENV_ROOT/shims
-end
-
-
-# Hooks
-
-## direnv
-type -q direnv; and eval (direnv hook fish)
-
-## pyenv
-if type -q pyenv; and status --is-interactive
-    pyenv init - | source
-end
-
-
 # Abbrev
 
 if status --is-interactive
@@ -54,13 +6,74 @@ if status --is-interactive
     abbr --add g 'git'
 end
 
+# Android
+set -gx ANDROID_HOME $HOME/Library/Android/sdk
+if test -d $ANDROID_HOME
+    fish_add_path -a $ANDROID_HOME/platform-tools $ANDROID_HOME/tools $ANDROID_HOME/tools.bin
+end
 
-# Global config
+# direnv
+if type -q direnv
+    eval (direnv hook fish)
+end
+
+# EDITOR
+if type -q bbedit
+    set -gx EDITOR bbedit --wait --resume
+end
+
+# Gunk
+
+set -gx COCOAPODS_DISABLE_STATS true
+set -gx FASTLANE_SKIP_UPDATE_CHECK 1
+
+# Go
+
+set -gx GOPATH $HOME/.go
+fish_add_path -p $GOPATH/bin
+
+# Homebrew
+
+if test -d /opt/homebrew
+    set -g -x HOMEBREW_PREFIX /opt/homebrew
+    fish_add_path -p $HOMEBREW_PREFIX/bin $HOMEBREW_PREFIX/sbin
+    set -gx HOMEBREW_CELLAR $HOMEBREW_PREFIX/Cellar
+    set -gx HOMEBREW_REPOSITORY $HOMEBREW_PREFIX
+    set -gx HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK 1
+    set -gx HOMEBREW_NO_ANALYTICS 1
+end
+
+# MacPorts
+
+fish_add_path -p /opt/local/bin /opt/local/sbin
+
+# pyenv
+
+set -gx PYENV_ROOT $HOME/.pyenv
+if test -d $PYENV_ROOT
+    fish_add_path -p $PYENV_ROOT/bin $PYENV_ROOT/shims
+    if type -q pyenv; and status --is-interactive
+        # too slow!
+        #pyenv init - | source
+    end
+end
+
+# ripgrep
 
 set -gx RIPGREP_CONFIG_PATH $HOME/.ripgreprc
 
+# rubygems
 
-# Local config
+set -gx GEM_HOME $HOME/.gem
+if type -q ruby
+    # too slow! faster way?
+    #fish_add_path (ruby -r rubygems -e 'puts Gem.user_dir')/bin
+end
+
+# local_config
 
 set -l local_config $HOME/.local_config.fish
-test -f $local_config; and source $local_config
+if test -f $local_config
+    source $local_config
+end
+
